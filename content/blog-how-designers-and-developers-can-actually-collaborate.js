8bf86fb4a07cd2f95ac8163a385dbd6ff85aecc7
darya499 <deswork107@gmail.com>
2026-09-18 10:21:02 +0300

    Add lightweight page content overrides

---
+++ content/blog-how-designers-and-developers-can-actually-collaborate.js
@@ -0,0 +1,4 @@
+/* Content overrides for /blog/how-designers-and-developers-can-actually-collaborate/. Edit this small file for page-specific content changes. */
+window.SitePageContent = {
+  replacements: []
+};
